e632422a9f66368899d8d0786295f72ff79638d5
Ella <hui.liu4@se.com>
2024-04-10 14:37:12 +0800

    Initial Commit

---
+++ myGHfollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myGHfollowers
+//
+//  Created by Ella on 2024/4/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
